bbd5c9a6e836d7589c9307ad0887cda23ea7cf98
Nancy Verma <nancy.verma0591@gmail.com>
2025-12-10 20:39:19 +0530

    Second Commit from GitDemo

---
+++ src/test/java/org/example/Tests/RestAPITest.java
@@ -0,0 +1,11 @@
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+
+    // this is to explain git
+
+    @Test
+    public void postJira(){
+        System.out.println("postJira");
+    }
+}
